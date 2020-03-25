2aeef11297f476c560bac91a53e0aec51bb91f71
fdnklg <fabian.dinklage@me.com>
2020-03-25 13:59:10 +0100

    (logic) StoreProvider added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {StoreProvider} from 'easy-peasy';
 
+import Store from "state/Store";
 import Appwrapper from 'components/';
 
 ReactDOM.render(
-  <Appwrapper />,
+  <StoreProvider store={Store}>
+    <Appwrapper />
+  </StoreProvider>
+  ,
   document.getElementById('root')
 );
